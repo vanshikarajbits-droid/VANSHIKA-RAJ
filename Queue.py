9a4aba75605c9a00661ec734176b953fe6f46827
vanshikarajbits-droid <vanshikaraj.bits@gmail.com>
2026-03-23 20:23:36 +0530

    Create Queue.py

---
+++ Queue.py
@@ -0,0 +1,15 @@
+
+queue = []
+
+# Enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue after enqueue:", queue)
+
+# Dequeue
+removed = queue.pop(0)
+print("Removed element:", removed)
+
+print("Queue after dequeue:", queue)
