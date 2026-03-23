23f65f7fd3eae4002252ba7dd5815cc532827a29
vanshikarajbits-droid <vanshikaraj.bits@gmail.com>
2026-03-23 20:24:48 +0530

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,16 @@
+
+
+stack = []
+
+# Push
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack after push:", stack)
+
+# Pop
+removed = stack.pop()
+print("Removed element:", removed)
+
+print("Stack after pop:", stack)
